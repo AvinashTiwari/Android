3b98d4f1e858706aebd91501bc6e4c8968c20c4f
AvinashTiwari85 <qwe123kids@gmail.com>
2020-12-07 17:01:39 -0700

    no message

---
+++ 8_layouts/App1/app/src/main/java/learn/android/myapplication/MainActivity2.kt
@@ -0,0 +1,11 @@
+package learn.android.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
